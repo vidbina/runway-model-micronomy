cb41ee767f6ae34900195de28ef34c2cf5fde3f3
David Asabina <david@supr.nu>
2016-05-20 16:58:35 +0200

    Enforce style + simplifcation of case

--- micronomy.js
+++ micronomy.js
@@ -33,40 +33,32 @@ let View = function(controller, svg, module) {
 
   svg.append('g').attr('id', 'experiment');
 
-  let actors = [
-    new DummyActor('#experiment'),
-    new DummyActor('#experiment'),
-  ];
-
   let model = module.env;
-  let nodes = [{}, {}, {}, {}, {}, {}, {}, {}];
-  console.log("   nodes", nodes);
-  let simnodes = _nodes(model);
-  console.log("simnodes", simnodes);
-  let links = [
-    {source: 0, target: 1},
-    {source: 1, target: 2},
-    {source: 0, target: 3},
-    {source: 0, target: 4},
-    {source: 5, target: 4},
-    {source: 6, target: 4},
-  ];
 
-  let messages = [
-    {id: 0, link: 0, progress: 0.5, dir: true, col: 0},
-    {id: 1, link: 2, progress: 0, dir: true, col: 0},
-    {id: 2, link: 4, progress: 0.5, dir: true, col: 0},
-    {id: 3, link: 4, progress: 1, dir: true, col: 0},
-  ];
+  let nodes = [];
+  let links = [];
+  let messages = [];
 
-  let node_size = floor(width, height)/(nodes.length*5);
-  let edge_size = node_size*3;
+  const _nodeSize = (_n) => {
+    return 12;
+    //return floor(width, height)/(nodes.length<0?10:nodes.length*5);
+  };
+
+  const _edgeLength = (_e) => {
+    console.log(_e);
+    return 24;
+    //return _nodeSize()*3;
+  };
+
+  const _messageSize = (_m) => {
+    return 10;
+  };
 
   let force = d3.layout.force()
     .size([width/2, height/2])
     .nodes(nodes)
     .links(links)
-    .linkDistance(edge_size);
+    .linkDistance(_edgeLength);
 
   let link = svg.selectAll('.link')
     .data(links)
@@ -74,29 +66,24 @@ let View = function(controller, svg, module) {
     .attr('class', 'edge')
     .style(style.link);
 
-  let node = svg.selectAll('.node')
-    .data(nodes)
-    .enter().append('circle')
-    .attr('class', 'node')
-    .attr('r', node_size)
-    .style(style.node)
-    .call(force.drag);
+  let node = svg.selectAll('.node');
+
+  let _start = () => {
+    console.log('nodes', force.nodes());
+    node = node.data(force.nodes());
+    node.enter().append('circle')
+      .attr('class', 'node')
+      .attr('r', _nodeSize)
+      .style(style.node)
+      .call(force.drag);
+    force.start();
+  };
 
   let message = svg.selectAll('.messages')
     .data(messages)
     .enter().append('circle')
-    .attr('r', node_size*2/3)
+    .attr('r', _messageSize)
     .style(style.message);
-
-  /*
-  console.log("link", link);
-  console.log("data", link.data());
-  for(var x in link.data()) { console.log("x", x); };
-  console.log("data[0]", link.data());
-  console.log("data", link.data());
-  console.log("datum", link.datum());
-  console.log(messages[0], link, node)
-  */
 
   force.on('tick', () => {
     node
@@ -113,17 +100,17 @@ let View = function(controller, svg, module) {
     message
       .attr('cx', function(d) {
         if (messages[d.id].dir == true) {
-          if(messages[d.id].progress >= 1) { messages[d.id].dir = false }
-          messages[d.id].progress = messages[d.id].progress+0.1
+          if(messages[d.id].progress >= 1) { messages[d.id].dir = false; }
+          messages[d.id].progress = messages[d.id].progress+0.1;
         } else {
-          if(messages[d.id].progress <= 0) { 
+          if(messages[d.id].progress <= 0) {
             messages[d.id].dir = true;
             messages[d.id].col = d3.rgb(Math.random()*255, Math.random()*255, Math.random()*255).toString();
           }
-          messages[d.id].progress = messages[d.id].progress-0.1
+          messages[d.id].progress = messages[d.id].progress-0.1;
         }
         let _l = link.data()[d.link];
-        let _x = _l.source.x + (_l.target.x - _l.source.x)*d.progress
+        let _x = _l.source.x + (_l.target.x - _l.source.x)*d.progress;
         return(_x);
       })
       .attr('cy', function(d) {
@@ -142,62 +129,11 @@ let View = function(controller, svg, module) {
   return({
     wideView: true,
     update: () => {
-      for(var actor of actors) {
-        actor.update();
-      }
+      let simnodes = _nodes(model);
+      _start();
     }
   });
 };
-
-// Actors should append an item to the container identified by `groupID` and
-// update this element during the update call.
-class Actor {
-  constructor(groupID) {
-    this._uuid = `uuid${uuid()}`;
-  }
-
-  get uuid() {
-    return this._uuid;
-  }
-
-  get node() {
-    return d3.select(`#${this.uuid}`).node();
-  }
-}
-
-class DummyActor extends Actor {
-  constructor(groupID) {
-    super(groupID);
-    this.r = 1;
-    this.expand = true;
-
-    // NOTE: Somehow rect here is inaccurate but a resize call is made right
-    // after initialization
-    let node = d3.select(groupID)
-      .append('circle')
-        .attr('id', this.uuid)
-        .attr('r', this.r)
-  }
-
-  update() {
-    if(this.r > 100) { this.expand = false; }
-    if(this.r < 10) { this.expand = true; }
-
-    if(this.expand) {
-      this.r = this.r + 1;
-    } else {
-      this.r = this.r - 1;
-    }
-
-    let svg = d3.select('svg');
-    const rect = svg.node().viewBox.baseVal;
-
-    d3.select('#experiment').select('circle')
-      .attr('r', this.r);
-      //.attr('cx', rect.width/2)
-      //.attr('cy', rect.height/2);
-  }
-}
 
 // Helpers
 const uuid = () => {
@@ -218,7 +154,8 @@ const floor = (a, b) => {
 
 const _nodes = (model) => {
   return model.vars.get('parties').toJSON().map(x => {
-    return {}; // id: x[0] };
+    console.log(x[1], x[0]);
+    return x[1];
   });
 };
 
